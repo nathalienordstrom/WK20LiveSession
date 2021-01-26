295d48414594c3a71685df1c8b8892fff5902014
nathalie <nathalie-nordstrom@hotmail.com>
2021-01-26 18:05:24 +0100

    login and out

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import crypto from "crypto";
 import bcrypt from "bcrypt-nodejs";
 
+
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
@@ -33,6 +34,7 @@ const User = mongoose.model("User", {
   accessToken: {
     type: String,
     default: () => crypto.randomBytes(128).toString("hex"),
+    unique: true,
   },
 });
 
@@ -48,15 +50,43 @@ const authenticateUser = async (req, res, next) => {
   next();
 };
 
-// Create user  - sign up
-app.post("/users", async (req, res) => {});
+// Sign-up
+app.post("/users", async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    const SALT = bcrypt.genSaltSync(10)
+    const user = await new User({ 
+      name, 
+      password: bcrypt.hashSync(password, SALT),
+    }).save();
+    res.status(200).json({ userId: user._id, accessToken: user.accessToken });
+  } catch (err) {
+    res.status(400).json({ message: "Could not create user", errors: err });
+
+  }
+});
+
+// Login
+app.post("/sessions", async (req, res) => {
+  try{
+    const {name, password } = req.body;
+    const user = await User.findOne({name});
+    if(user && bcrypt.compareSync(password, user.password)){
+        res.status(200).json({ userId: user._id, accessToken: user.accessToken })
+    }else{
+      throw "User not found";
+    }
+  }catch(err){
+    res.status(404).json({error:err})
+  }
+  res.status(501).send();
+});
+
+
 
 // Secure endpoint, user needs to be logged in to access this.
 app.get("/users/:id", authenticateUser);
-app.get("/users/:id", (req, res) => {});
-
-// login user
-app.post("/sessions", async (req, res) => {});
+app.get("/users/:id", (req, res) => { });
 
 // Start the server
 app.listen(port, () => {
